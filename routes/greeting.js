531fcfa6828725cf2fcd57fa181949f576a64c79
Jody LeCompte <jody@jodylecompte.com>
2018-06-13 21:57:32 -0500

    Finalized code

--- routes/greeting.js
+++ routes/greeting.js
@@ -1,7 +1,23 @@
 module.exports = {
     hello: function(req, res) {
         if (!req.body.name) {
-            res.send('An error occurred: Name is a required paramter');
+            return res.send('An error occurred: Name is a required paramter');
+        }
+
+        const name = req.body.name;
+        const lang = req.body.language || 'en';
+        switch(lang) {
+            case 'en':
+                return res.send('Hello, ' + name)
+            break;
+            case 'es':
+                return res.send('Hola, ' + name)
+            break;
+            case 'de':
+                return res.send('Hallo, ' + name)
+            break;
+            default:
+                return res.send('Error: Invalid Language')
         }
     }
 };
--- tests/routes/greeting.test.js
+++ tests/routes/greeting.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
-const { hello, goodbye } = require('../../routes/greeting');
+const { hello } = require('../../routes/greeting');
 
 let req = {
     body: {},
@@ -18,6 +18,40 @@ describe('Greetings Route', function() {
         it('Should error out if no name provided ', function() {
             hello(req, res);
             expect(res.sendCalledWith).to.contain('error');
+        });
+
+        it('Should respond in English as default', function() {
+            let newReq = req;
+            newReq.body.name = 'Jody';
+            
+            hello(newReq, res);
+            expect(res.sendCalledWith).to.equal('Hello, Jody');
+        });
+
+        it('Should error on invalid language', function() {
+            let newReq = req;
+            newReq.body.name = 'Jody';
+            newReq.body.language = 'bad-input';
+            
+            hello(newReq, res);
+            expect(res.sendCalledWith).to.equal('Error: Invalid Language');
+        });
+
+        it('Should return greeting for english, spanish, or german', function() {
+            let newReq = req;
+            newReq.body.name = 'Jody';
+
+            newReq.body.language = 'en';
+            hello(newReq, res);
+            expect(res.sendCalledWith).to.equal('Hello, Jody');
+
+            newReq.body.language = 'es';
+            hello(newReq, res);
+            expect(res.sendCalledWith).to.equal('Hola, Jody');
+
+            newReq.body.language = 'de';
+            hello(newReq, res);
+            expect(res.sendCalledWith).to.equal('Hallo, Jody');
         });
     })
 });
